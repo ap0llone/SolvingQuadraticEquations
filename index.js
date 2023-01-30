0de568d7b72eadca0c8e96d2d84ee5b7331d4473
Abbas Ibragimov <120020090+ap0llone@users.noreply.github.com>
2023-01-30 23:11:27 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function discre(a,b,c) {
+    let d = Math.pow(b,2) - 4 * a * c;
+    let x1 = (-b + Math.sqrt(d)) / 2 * a;
+    let x2 = (-b - Math.sqrt(d)) / 2 * a;
+    if(d < 0){
+        return 'There is no solution';
+    }
+    else if(d == 0){
+        return (-b / 2 * a)
+    }
+    else{
+        return `x1 = ${x1}  x2 = ${x2}`;
+    }
+}
